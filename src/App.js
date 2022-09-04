33070aa1c7395d38f60d01c1e3ae2381f5a7ccfa
Nemo21 <saggubableen9@gmail.com>
2022-09-04 14:43:26 +0530

    Body Added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Nav from './components/Nav';
+import React from "react";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Nav/>
-      <Hero/>
-      <Footer/>
+      <Header />
+      <Nav />
+      <Hero />
+      <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Body.jsx
@@ -0,0 +1,295 @@
+import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BiComment } from "react-icons/bi";
+import "./Body.css";
+
+function Body() {
+  return (
+    <div className="Body">
+      <div className="Container">
+        <div className="Left-Black">
+          <div className="big-card1">
+            <img
+              className="cardimage"
+              src="https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <img
+              className="cardimage"
+              src="https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <img
+              className="cardimage"
+              src="https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <img
+              className="cardimage"
+              src="https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+            />
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="Center-White">
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="Right-Black">
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="small-card1">
+            <div className="content">
+              <h3>Topic,Date</h3>
+              <br></br>
+              <h2>Heading</h2>
+              <br></br>
+              <p>Information</p>
+              <br></br>
+              <span className="heart-text">
+                <AiOutlineHeart className="heart" />
+                68
+              </span>
+              <span className="comment-text">
+                <BiComment className="comment" />
+                128
+              </span>
+            </div>
+          </div>
+          <div className="content">
+            <h3>Topic,Date</h3>
+            <br></br>
+            <h2>Heading</h2>
+            <br></br>
+            <p>Information</p>
+            <br></br>
+            <span className="heart-text">
+              <AiOutlineHeart className="heart" />
+              68
+            </span>
+            <span className="comment-text">
+              <BiComment className="comment" />
+              128
+            </span>
+          </div>
+          <div className="content">
+            <h3>CAPSs 12.09.3444</h3>
+            <br></br>
+            <h2>asffmci j iemc akk oenfmmci jjevnpemm dnefnnai</h2>
+            <br></br>
+            <p>
+              Use the opacity property to add transparency to a button (creates
+              a "disabled" look). Tip: You can also add the cursor property with
+              a value of "not-allowed", which will display a "no parking sign"
+              when you mouse over the button:
+            </p>
+            <br></br>
+            <span className="heart-text">
+              <AiOutlineHeart className="heart" />
+              68
+            </span>
+            <span className="comment-text">
+              <BiComment className="comment" />
+              128
+            </span>
+            <button className="moreconvo">More Conversation</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Body;
